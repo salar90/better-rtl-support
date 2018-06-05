b02bcdb0fd60395f2f0cdac14aec80c85446fa42
Salar Gholizadeh <salar.g@gmail.com>
2018-06-05 18:19:53 +0430

    change function prefix to BRTL_

--- better-rtl-themes.php
+++ better-rtl-themes.php
@@ -15,16 +15,16 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-add_filter("body_class","sg_betterrtl_add_body_classes");
+add_filter("body_class","BRTL_add_body_classes");
 
-function sg_betterrtl_add_body_classes($class){
+function BRTL_add_body_classes($class){
     $class[]= "theme-" .get_template();
     return $class;
 }
 
 
-function sg_betterrtl_enqueue_iransans_fonts() {
+function BRTL_enqueue_iransans_fonts() {
 
     wp_enqueue_style( 'iransans', plugins_url("assets/style.css",__FILE__), array( ));
 }
-add_action( 'wp_enqueue_scripts', 'sg_betterrtl_enqueue_iransans_fonts',100 );
+add_action( 'wp_enqueue_scripts', 'BRTL_enqueue_iransans_fonts',100 );

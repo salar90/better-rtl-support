f02f1b680fa795b7be962da5afb381de56e9aa75
Salar Gholizadeh <salar.g@gmail.com>
2018-06-05 19:12:02 +0430

    make object oriented

---
+++ better-rtl-support.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Better RTL Support
+Description: Make Themes & plugins more readable in Persian language by changing fonts and tweaking styles.
+Author: Salar Gholizadeh
+Version: 1.0
+Author URI: http://salar.one
+Plugin URI: https://github.com/salar90/better-rtl-support
+License:     GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+Text Domain: better-rtl
+Domain Path: /languages
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+require_once "class-better-rtl-support.php";
---
+++ class-better-rtl-support.php
@@ -0,0 +1,26 @@
+<?php
+class better_RTL_support{
+    
+    function __construct()
+    {
+
+    }
+    function assign_hooks(){
+
+        add_filter("body_class",[$this,"add_body_classes"]);
+        add_action( 'wp_enqueue_scripts', [$this,"enqueue_styles"],50 );
+    }
+
+    
+
+    function add_body_classes($class){
+        $class[]= "theme-" .get_template();
+        return $class;
+    }
+    
+    
+    function enqueue_styles() {
+        wp_enqueue_style( 'brtls-styles', plugins_url("assets/style.css",__FILE__), array( ));
+    }
+    
+}

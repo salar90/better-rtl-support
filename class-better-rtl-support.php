7e36e708384f4c62964a58d86011c44d6d70e82a
Salar Gholizadeh <salar.g@gmail.com>
2018-06-08 16:43:54 +0430

    savable plugin options

--- class-better-rtl-support.php
+++ class-better-rtl-support.php
@@ -9,9 +9,9 @@ class better_RTL_support{
     function assign_hooks(){
         add_filter("body_class",[$this,"add_body_classes"]);
         add_action( 'wp_enqueue_scripts', [$this,"enqueue_styles"],50 );
-
         add_action("admin_menu",[$this,"admin_menu"]);
 
+        add_action( 'admin_init', array( $this, 'settings_init' ) );
 
     }
 
@@ -19,11 +19,15 @@ class better_RTL_support{
     function admin_menu(){
         add_theme_page(__("RTL Settings","better-rtl"), __("RTL Settings","better-rtl"), "manage_options" , "rtl-settings" , [$this, "settings_page"] );
     }
+
+    function settings_init(){
+        register_setting( 'better-rtl-general', 'font_iransans_path' );
+        register_setting( 'better-rtl-general', 'font_iransans_license' );
+    }
     
     function settings_page(){
         include "settings-page.php";
     }
-
 
     function add_body_classes($class){
         $class[]= "theme-" .get_template();
@@ -34,5 +38,5 @@ class better_RTL_support{
     function enqueue_styles() {
         wp_enqueue_style( 'brtls-styles', plugins_url("assets/style.css",__FILE__), array( ));
     }
-    
+
 }
--- settings-page.php
+++ settings-page.php
@@ -11,31 +11,33 @@ if (!defined('ABSPATH')) {
 			<a href="http://fontiran.com" rel="external nofollow" target="_blank">fontiran.com</a>
 			مراجعه نمایید.
 		</p>
-		<table class="form-table">
-			<tbody>
-				<tr>
-					<th scope="row">
-						<label for="iransans-lisence">کد لایسنس فونت ایران‌سنس</label>
-					</th>
-					<td>
-						<input type="text" name="iransans-lisence" id="iransans-lisence">
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">
-						<label for="fonts-path">آدرس URL برای پوشه "fonts"</label>
-						<p>این پوشه را می توانید در پوشه WebFonts در بسته دانلودی خود از سایت فونت ایران پیدا کنید.</p>
-					</th>
-					<td>
-						<input type="text" id="fonts-path" name="fonts-path" placeholder="example.com/fonts/">
-					</td>
-				</tr>
+		<form action="options.php" method="POST">
+    <?php settings_fields( 'better-rtl-general' ); ?>
+    <?php do_settings_sections( 'better-rtl-general' ); ?>
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row">
+							<label for="iransans-license">کد لایسنس فونت ایران‌سنس</label>
+						</th>
+						<td>
+							<input value="<?php echo esc_attr( get_option('font_iransans_license') ); ?>" type="text" name="font_iransans_license" id="iransans-lisence">
+						</td>
+					</tr>
+					<tr>
+						<th scope="row">
+							<label for="fonts-path">آدرس URL برای پوشه "fonts"</label>
+							<p>این پوشه را می توانید در پوشه WebFonts در بسته دانلودی خود از سایت فونت ایران پیدا کنید.</p>
+						</th>
+						<td>
+							<input value="<?php echo esc_attr( get_option('font_iransans_path') ); ?>" type="text" id="fonts-path" name="font_iransans_path" placeholder="example.com/fonts/">
+						</td>
+					</tr>
 
-			</tbody>
-		</table>
+				</tbody>
+			</table>
 
-		<p class="submit">
-			<input type="submit" name="submit" id="submit" class="button button-primary" value="ذخیره‌ی تغییرات">
-		</p>
-
+    		<?php submit_button(); ?>
+		</form>
+		
 	</div>

5643421c55a9777689dbbe95d22dc7b9abfc1369
Salar Gholizadeh <salar.g@gmail.com>
2023-09-16 15:51:56 +0330

    remove iransans-related code

--- class-better-rtl-support.php
+++ class-better-rtl-support.php
@@ -19,8 +19,6 @@ class better_RTL_support
             add_action("admin_menu", [$this, "admin_menu"]);
             add_action('admin_init', array($this, 'settings_init'));
         }
-
-        add_action("wp_head", [$this, "display_license_header"]);
     }
 
     function load_textdomain()
@@ -55,31 +53,6 @@ class better_RTL_support
     function enqueue_styles()
     {
         wp_enqueue_style('brtls-styles', plugins_url("assets/style.css", __FILE__), array());
-        wp_add_inline_style('brtls-styles', $this->generate_font_face_css("iransans"));
-    }
-
-    function generate_font_face_css($font_face)
-    {
-        //Todo: sanitize $font_face, for now static value won't harm.
-        ob_start();
-
-        $face_path = get_option("font_iransans_path");
-        $license_code = trim(esc_html(get_option("font_iransans_license")));
-
-        if (!empty($face_path)) {
-            include __DIR__ . "/font-faces/" . $font_face . ".php";
-        }
-
-        $css = ob_get_clean();
-
-        return $css;
-    }
-
-    function display_license_header()
-    {
-        $license = trim(esc_html(get_option("font_iransans_license")));
-        if (!empty($license)) {
-            echo "\n<meta name=\"fontiran.com:license\" content=\"$license\">\n";
-        }
+        wp_add_inline_style('brtls-styles', ":root{--brtls-font-text: 'Vazirmatn', sans-serif;}");
     }
 }
